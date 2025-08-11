62384f4c37277a01a11209dfe4ce6858bfab238e
Nicholas Barrow <nbarrow@inspiretmstech.com>
2025-08-11 00:23:32 -0400

    feat: test auto-build

--- internal/server/http.go
+++ internal/server/http.go
@@ -2,7 +2,9 @@ package server
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"github.com/getkin/kin-openapi/openapi3filter"
 	"github.com/gin-gonic/gin"
 	ginvalidator "github.com/oapi-codegen/gin-middleware"
 	"github.com/train360-corp/projconf/internal/server/api"
@@ -27,7 +29,24 @@ func NewHTTPServer(cfg Config) (*HTTPServer, error) {
 	r.Use(gin.Recovery())
 
 	swagger := api.MustSpec()
-	r.Use(ginvalidator.OapiRequestValidator(swagger))
+	opts := &ginvalidator.Options{}
+	opts.Options.AuthenticationFunc = func(ctx context.Context, input *openapi3filter.AuthenticationInput) error {
+
+		req := input.RequestValidationInput.Request
+		id := req.Header.Get("x-client-secret-id")
+		sec := req.Header.Get("x-client-secret")
+
+		if id == "" {
+			return errors.New("missing 'x-client-secret-id' header")
+		} else if sec == "" {
+			return errors.New("missing 'x-client-secret' header")
+		}
+
+		// TODO: attempt to get the client to verify if the authentication succeeds
+
+		return errors.New("authentication not implemented")
+	}
+	r.Use(ginvalidator.OapiRequestValidatorWithOptions(swagger, opts))
 
 	routes := &RouteHandlers{}
 	api.RegisterHandlers(r, routes)
